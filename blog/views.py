98cfb5fb26a29b9c2174459adca6fca0a107652b
HannaGfrorer Gfrörer <hannagfrorer@gmail.com>
2023-05-03 09:56:22 +0000

    add code to index.html and som css

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.views import generic
-from .model import Post
+from .models import Post
 
 
 class PostList(generic.ListView):
     model = Post
-    queryset = Post.objects.filter(status=1).order_by('-created_on')
-    template_name = 'index.html'
-    praginate_by = 6
+    queryset = Post.objects.filter(status=1).order_by("-created_on")
+    template_name = "index.html"
+    paginate_by = 6
+
